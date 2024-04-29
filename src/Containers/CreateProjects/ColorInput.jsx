e02d8ab91d4fc4cb06fd3196f9ba2526aaabc30a
evgluzedev <evgeniy.shostak.04@mail.ru>
2024-04-29 16:27:36 +0300

    added new color input component

--- src/Containers/CreateProjects/ColorInput.jsx
+++ src/Containers/CreateProjects/ColorInput.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { MuiColorInput } from 'mui-color-input'
 import { Box } from '@mui/material'
-
+import DialogContext from '../../Components/DialogContext'
+import {TwitterPicker} from 'react-color'
 function ColorInput() {
+    const {data,setDataHandler} = useContext(DialogContext)
 
-    const [value, setValue] = React.useState('#ffffff')
-
-    const handleChange = (newValue) => {
-      setValue(newValue)
+  
+    const handleChange = (value) => {
+      
+      setDataHandler({...data,color:value.hex})
     }
 
     return ( 
 
         <Box  sx={{maxWidth:"200px"}}>
-                <MuiColorInput format="hex" value={value} onChange={handleChange} />
+                {/* <MuiColorInput format="hex" value={data.color || "#ffffff"}  onInput={handleChange}/> */}
+                <TwitterPicker onChangeComplete={handleChange} />
         </Box>
 
     
