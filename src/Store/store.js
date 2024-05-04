c47e41b531b01c1f57e9bc004c9cfeeca371be8a
evgluzedev <evgeniy.shostak.04@mail.ru>
2024-05-04 20:29:42 +0300

    added project slice

--- src/Store/store.js
+++ src/Store/store.js
@@ -2,11 +2,13 @@ import {configureStore} from "@reduxjs/toolkit"
 import appUserReducer from '../Store/slices/appUserSlice'
 import companyReducer from '../Store/slices/companySlice'
 import workersSlice from "./slices/workersSlice"
+import projectSlice from "./slices/projectSlice"
 export default configureStore({
     reducer:{
         appUser:appUserReducer,
         company:companyReducer,
-        workers:workersSlice
+        workers:workersSlice,
+        projects:projectSlice
     },
    
 })
