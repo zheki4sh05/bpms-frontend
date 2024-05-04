d18500880b3eb41120ffc21522af27218fdcfe5d
evgluzedev <evgeniy.shostak.04@mail.ru>
2024-05-04 20:28:51 +0300

    made project header active

---
+++ src/Components/ProjectsPage/ProjectsPageHeader.jsx
@@ -0,0 +1,41 @@
+import { useSelector } from "react-redux";
+import PageInfo from "../PageInfo";
+import {
+  getActiveProjectsCount,
+  getAdminProjectsCount,
+  getCountOverdueProjectsCount,
+  getParticipantProjectsCount,
+} from "../../Store/slices/projectSlice";
+
+function ProjectsPageHeader() {
+  let countActive = useSelector(getActiveProjectsCount);
+  let countAdmin = useSelector(getAdminProjectsCount);
+  let countParticipant = useSelector(getParticipantProjectsCount);
+  let countOverdue = useSelector(getCountOverdueProjectsCount);
+
+  return (
+    <PageInfo
+      name={"Мои проекты"}
+      data={[
+        {
+          name: "активно",
+          count: countActive,
+        },
+        {
+          name: "управляю",
+          count: countAdmin,
+        },
+        {
+          name: "состою:",
+          count: countParticipant,
+        },
+        {
+          name: "просрочено:",
+          count: countOverdue,
+        },
+      ]}
+    />
+  );
+}
+
+export default ProjectsPageHeader;
