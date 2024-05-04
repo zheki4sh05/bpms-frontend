cf29a5642b4c9123716568ac88e8394cf11887bd
evgluzedev <evgeniy.shostak.04@mail.ru>
2024-05-04 20:30:46 +0300

    modify

--- src/Components/CreateProject.jsx
+++ src/Components/CreateProject.jsx
@@ -5,15 +5,38 @@ import WorkersListControl from "../Containers/CreateProjects/WorkersListControl"
 import CreateEntity from "./CreateEntity";
 import { useContext } from "react";
 import DialogContext from './DialogContext';
+import { useDispatch } from 'react-redux';
+import { useSelector } from "react-redux";
+import { getToken } from "../Store/slices/appUserSlice";
+import { createProject, getCreatedStatus } from "../Store/slices/projectSlice";
+import statusTypes from "../API/status";
 
 
 export default function CreateProject() {
 
-  const {data, getDialogResult,resetDialogContext,setDataHandler} = useContext(DialogContext)
+  const {data, getDialogResult,resetDialogContext} = useContext(DialogContext);
 
+  const createdStatus = useSelector(getCreatedStatus);
+
+  const dispatch = useDispatch();
+
+  const token = useSelector(getToken)
 
   const handleSaveCreatedProject=()=>{
-    //задиспатчить сохранение
+ 
+    dispatch(createProject({
+      project:{
+        ...data.aboutProject,
+        ...data.deadline,
+        ...data.members
+      },
+      token
+
+    }))
+  }
+
+  if(createdStatus===statusTypes.succeeded){
+    resetDialogContext();
   }
 
   return (
